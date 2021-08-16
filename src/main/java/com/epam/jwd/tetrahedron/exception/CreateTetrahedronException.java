af818ce3afaac44b9f63cfaa413c800e09ddca61
Roman Gospodynich <fossibata@gmail.com>
2021-08-16 12:23:33 +0300

    Add CreateTetrahedronException.java

---
+++ src/main/java/com/epam/jwd/tetrahedron/exception/CreateTetrahedronException.java
@@ -0,0 +1,9 @@
+package com.epam.jwd.tetrahedron.exception;
+
+public class CreateTetrahedronException extends Exception{
+    public CreateTetrahedronException() {
+    }
+    public CreateTetrahedronException(String message) {
+        super(message);
+    }
+}
