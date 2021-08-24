22020b203d3d8914630b0fa0380d817f5b9999e6
Roman Gospodynich <fossibata@gmail.com>
2021-08-24 18:25:37 +0300

    Format classes in compatator

--- src/main/java/com/epam/jwd/tetrahedron/comparator/IdComparator.java
+++ src/main/java/com/epam/jwd/tetrahedron/comparator/IdComparator.java
@@ -5,6 +5,7 @@ import com.epam.jwd.tetrahedron.model.TetrahedronRegistrar;
 import java.util.Comparator;
 
 public class IdComparator implements Comparator<TetrahedronRegistrar> {
+
     @Override
     public int compare(TetrahedronRegistrar o1, TetrahedronRegistrar o2) {
         return o1.getId() - o2.getId();
